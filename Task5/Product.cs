f3b241a863ef7e539c5cd3110caa1ad243791c6d
ktcliff <cliffkri@kean.ed>
2023-02-15 21:20:01 -0500

    test

---
+++ Task5/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace Task5;
+public class Product
+{
+
+        /// <summary>
+        /// This method calculates the product of two numbers
+        /// </summary>
+        /// <param name="a">The first number</param>
+        /// <param name="b">The second number</param>
+        /// <returns>The product of the two numbers</returns>
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// This method calculates the product of three numbers
+        /// </summary>
+        /// <param name="a">The first number</param>
+        /// <param name="b">The second number</param>
+        /// <param name="c">The third number</param>
+        /// <returns>The product of the three numbers</returns>
+        public static double Multiply(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+
+}
